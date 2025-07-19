c7465c4b37bd346185bf54cdb91cd8132944f26c
Krunal-Jagtap <Krunalofficial11j@gmail.com>
2025-07-20 01:13:24 +0530

    Add Notes - Variables

---
+++ Notes/01_Variables.js
@@ -0,0 +1,26 @@
+// We can stored any value in Variables
+
+const userName = "Krunal";
+console.log(userName); // Krunal
+
+// Variable Name rules :
+// - Variable names are case sensitive
+console.log(userName);
+console.log(username); // username is not defined
+
+// - spaces noe allow in variable name
+const age = 10;
+console.log(age); // 10
+
+// const user number = 10 // ❗ERROR
+
+// - Variables cannot be the same as reserved keywords
+// const const = "hello"; // ❗ERROR 'const' is not allowed as a variable declaration name.
+
+// - Just keep this in mind
+// We can use underscore(_) and dollar($) sign in the start of variable
+
+// Best Practice - Variable Name Write in camelCase
+// - 1st word in small caps user
+// - 2nd word in Upper caps Name
+// - userName
