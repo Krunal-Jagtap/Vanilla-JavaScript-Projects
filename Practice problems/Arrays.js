b2ad63682438448c1cdbe087a94a62a9f4a485fc
Krunal-Jagtap <Krunalofficial11j@gmail.com>
2025-07-31 13:07:38 +0530

    Add How Arrays Works

---
+++ Practice problems/Arrays.js
@@ -0,0 +1,14 @@
+let fruits = ["Apple", "Banana", "Watermelon"];
+
+console.log(fruits); // [ 'Apple', 'Banana', 'Watermelon' ]
+
+// Index Start From 0
+console.log(fruits[0]); // Apple
+console.log(fruits[3]); // undefined
+
+// length Start From 1
+console.log(fruits.length); // 3
+
+// Change Element Apple to Mango
+fruits[0] = "Mango";
+console.log(fruits); // [ 'Mango', 'Banana', 'Watermelon' ]
