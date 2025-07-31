e662b0723d447e30e72c1d26b67bf51e9c0069f3
Krunal-Jagtap <Krunalofficial11j@gmail.com>
2025-07-31 13:09:40 +0530

    Add How For Loop Works

---
+++ Practice problems/Loop.js
@@ -0,0 +1,11 @@
+// For Loop :
+
+// Iteration = here start from 0
+// Limit = end of teas array
+// Increment ++ OR Decrement --
+
+let teas = ["Milk Tea", "Black Tea", "Special Tea"];
+
+for (let index = 0; index < teas.length; index++) {
+  console.log(`Index ${index} = ${teas[index]}`);
+}
