0a45dae72819bc95e1c72ca901c324cd09be3fc6
Krunal-Jagtap <Krunalofficial11j@gmail.com>
2025-08-01 12:39:46 +0530

    Add Count Number | While  Loop

--- Practice problems/Count_Numbers.js
+++ Practice problems/Count_Numbers.js
@@ -1,3 +1,13 @@
+// Using For Loop
 for (let index = 1; index <= 10; index++) {
   console.log(index);
+}
+
+// Using While Loop
+
+let number = 1;
+
+while (number <= 10) {
+  console.log(number);
+  number++;
 }
