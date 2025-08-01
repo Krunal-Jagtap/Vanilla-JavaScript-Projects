bed7c79ba646f8433459d8be03df348a3e7ae0f5
Krunal-Jagtap <Krunalofficial11j@gmail.com>
2025-08-01 12:37:59 +0530

    Add How While Loop Works

--- Practice problems/Loop.js
+++ Practice problems/Loop.js
@@ -9,3 +9,14 @@ let teas = ["Milk Tea", "Black Tea", "Special Tea"];
 for (let index = 0; index < teas.length; index++) {
   console.log(`Index ${index} = ${teas[index]}`);
 }
+
+// While Loop :
+// while (condition) {
+//     Code to be executed as long as the condition is true
+// }
+
+let i = 0;
+while (i <= teas.length) {
+  console.log(`Index ${i} = ${teas[i]}`);
+  i++;
+}
