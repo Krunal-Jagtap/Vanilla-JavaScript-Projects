2ddd11e53456ecd2ed0367bb5a468bbf7acf3919
Krunal-Jagtap <Krunalofficial11j@gmail.com>
2025-08-01 12:29:09 +0530

    Add Sum Of  Number | For Loop

---
+++ Practice problems/Sum_Of_Numbers.js
@@ -0,0 +1,14 @@
+let sum = 0;
+
+for (let index = 1; index <= 3; index++) {
+  sum = sum + index;
+}
+
+console.log(sum);
+
+// index 1
+// sum 0
+// index + sum = sum
+// 1 + 0 = 1
+// 2 + 1 = 3
+// 3 + 3 = 6
