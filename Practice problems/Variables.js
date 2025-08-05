77d4127989ac419a5410f1787ac385c3786d3f02
Krunal-Jagtap <Krunalofficial11j@gmail.com>
2025-08-05 13:42:34 +0530

    Add Notes | How we used varibale

---
+++ Practice problems/Variables.js
@@ -0,0 +1,11 @@
+// Variables - Use for Store value
+// Variables name prefer in camelCase
+
+const userName = "Krunal";
+console.log(userName);
+
+const _sendMessage = "Hi there we are learning JavaScript";
+console.log(_sendMessage);
+
+const $required = "Consistency";
+console.log($required);
