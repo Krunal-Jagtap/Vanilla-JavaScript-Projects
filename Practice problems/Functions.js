34d9061251d8379729483e63a652d1dac93fe235
Krunal-Jagtap <Krunalofficial11j@gmail.com>
2025-08-05 13:39:50 +0530

    Add Notes | How Functions  Works

---
+++ Practice problems/Functions.js
@@ -0,0 +1,32 @@
+// Function :
+// Block of code - reuseable
+
+function msgPopUp(leaning) {
+  console.log("Hello all, we are leaning " + leaning); // 'Hello all, we are leaning javaScript'
+}
+
+msgPopUp("javaScript");
+
+// Function with parameters
+
+function addition(a, b) {
+  return a + b;
+}
+
+console.log(addition(1, 1)); // 2
+
+// Anonymous Function :
+// Function without name
+
+let subtraction = function (a, b) {
+  console.log(a * b); // 4
+};
+
+subtraction(2, 2);
+
+// Arrow Function -
+const multiplication = (num1, num2) => {
+  return num1 * num2;
+};
+
+console.log(multiplication(2, 8)); // 16
