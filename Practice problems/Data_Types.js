96a6cfe834621676925f21aec5bb9da1af9a9008
Krunal-Jagtap <Krunalofficial11j@gmail.com>
2025-08-05 13:39:33 +0530

    Add Notes | Data Types

---
+++ Practice problems/Data_Types.js
@@ -0,0 +1,31 @@
+// Primitive Data types :
+
+// Number
+const userId = 1234;
+console.log(userId); // 1234
+console.log(typeof userId); // number
+
+// Strings
+const codeLanguage = "JavaScript";
+console.log(codeLanguage); // JavaScript
+console.log(typeof codeLanguage); // string
+
+// Boolean - return true or false
+const hasCard = true;
+console.log(hasCard); // true
+console.log(typeof hasCard); // boolean
+
+// Undefined - no value assign in it
+let userEmail;
+console.log(userEmail); // undefined
+console.log(typeof userEmail); // undefined
+
+// Null - Used later
+const userPhoneNumber = null;
+console.log(userPhoneNumber); // null
+console.log(typeof userPhoneNumber); // object
+
+// BigInt - big numbers - ( n )
+const userIndex = 9845198n;
+console.log(userIndex); // 9845198n
+console.log(typeof userIndex); // bigint
