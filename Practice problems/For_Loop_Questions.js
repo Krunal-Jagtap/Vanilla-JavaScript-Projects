8686a1af6f7ff7b6d5c78f05bedd25782c944ef0
Krunal-Jagtap <Krunalofficial11j@gmail.com>
2025-09-26 16:28:29 +0530

    Sum of numbers

--- Practice problems/For_Loop_Questions.js
+++ Practice problems/For_Loop_Questions.js
@@ -1,5 +1,12 @@
 // Table of any number
 
 for (let number = 1; number <= 10; number++) {
-  console.log(2 * number);
+  console.log(2 * number); // 2 4 6 8 10 12 14 16 18 20
 }
+
+// Sub of numbers
+let sumOfNumbers = 0;
+for (let index = 1; index <= 100; index++) {
+  sumOfNumbers += index;
+}
+console.log(sumOfNumbers); // 5050
