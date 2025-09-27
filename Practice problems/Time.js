b6db97eb415c99b4bf9c2f928758085a764ce4fe
Krunal-Jagtap <Krunalofficial11j@gmail.com>
2025-09-27 15:58:52 +0530

    JS Time | Methods

---
+++ Practice problems/Time.js
@@ -0,0 +1,14 @@
+// ========== Time ==========
+
+let time = new Date();
+console.log(time.getDate());
+
+console.log(time.getDay());
+
+console.log(time.getFullYear());
+
+console.log(time.getHours());
+
+console.log(time.getMinutes());
+
+console.log(time.getMonth());
