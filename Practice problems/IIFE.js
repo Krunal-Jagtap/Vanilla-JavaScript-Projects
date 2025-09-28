adc7c5d27ea9d153677b8033912c5e00cdf72bf7
Krunal-Jagtap <Krunalofficial11j@gmail.com>
2025-09-28 15:03:55 +0530

    IIFE | Function | Arrow | Parameters

--- Practice problems/IIFE.js
+++ Practice problems/IIFE.js
@@ -2,6 +2,23 @@
 
 // Function Without Name, Wrap in Parentheses, then called ();
 
+// Semi coleen ; used for end the Execution of the code.
+
+//Why IIFE =  Global scope variables does't not confused.
+
+// Use Case = Start Database connection.
+
+// Using Function
 (function () {
-  console.log(`I'm IIFE`);
-})(); // I'm IIFE
+  console.log(`DB CONNECTED PORT 1 🟢`);
+})(); // DB CONNECTED PORT 1 🟢
+
+// Using Arrow Function
+(() => {
+  console.log(`DB CONNECTED PORT 2 🟢`);
+})(); // DB CONNECTED PORT 2 🟢
+
+// Use with parameters
+((port) => {
+  console.log(`DB CONNECTED PORT ${port} 🟢`);
+})(3); // DB CONNECTED PORT 3 🟢
